797f3cc8381fcd610c2ee1636641db3f1de74b18
Melloman <lovelyboyy@mail.ru>
2022-11-14 00:53:50 +0300

    i

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashSet<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        HashSet<Integer> set2 = new HashSet<>();
+        set2.add(0);
+        set2.add(1);
+        set2.add(2);
+        System.out.println(symmetricDifference(set1, set2));
+    }
+    public static Set<Integer> symmetricDifference(Set<Integer> set1,Set<Integer> set2) {
+        HashSet result = new HashSet<>();
+        for (Integer s1 : set1) {
+            if (!set2.contains(s1)) {
+                result.add(s1);
+            }
+        }
+        for (Integer s2 : set2) {
+            if (!set1.contains(s2)) {
+                result.add(s2);
+            }
+        }
+        return result;
     }
 }
